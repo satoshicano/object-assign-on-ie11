01293f91cf5b15b8e4f2548a175b73c7bbba2737
satoshicano <satoshicano@gmail.com>
2017-10-11 21:09:24 +0900

    remove symbol

--- docs/dist.js
+++ docs/dist.js
@@ -2,17 +2,13 @@
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-var sym = Symbol();
-
 var obj1 = {
   "str": 1
 };
 
-var obj2 = _defineProperty({
+var obj2 = {
   "str": 2
-}, sym, 2);
+};
 
 var obj3 = _extends(obj1, obj2);
 
--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const sym = Symbol()
-
 const obj1 = {
  "str": 1,
 }
 
 const obj2 = {
  "str": 2,
- [sym]: 2 
 }
 
 const obj3 = Object.assign(obj1, obj2)
