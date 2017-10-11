c8ed8cd7713c4c76e30742c1866773d0b8260ad6
satoshicano <satoshicano@gmail.com>
2017-10-11 18:32:31 +0900

    migrate to babel-preset-env && use plugin

--- docs/dist.js
+++ docs/dist.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 var sym = Symbol();
@@ -12,7 +14,7 @@ var obj2 = _defineProperty({
   "str": 2
 }, sym, 2);
 
-var obj3 = Object.assign(obj1, obj2);
+var obj3 = _extends(obj1, obj2);
 
 if (obj1 === obj3) {
   var result = document.getElementById("result");
